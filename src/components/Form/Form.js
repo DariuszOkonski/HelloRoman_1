1a25b98ade2925e296d0286423999b209bc284a7
Dariusz Okoński <darek200180@gmail.com>
2023-04-18 17:22:49 +0200

    komponenty klasowe i statn - dodawanie css modules i scss

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,28 @@
+import './Form.css';
 import React from 'react';
 
 function Form({ submitFn }) {
   return (
-    <form onSubmit={submitFn}>
-      <input placeholder='name' name='name' type='text' />
-      <input placeholder='link' name='link' type='text' />
-      <input placeholder='image' name='image' type='text' />
-      <textarea placeholder='description' name='description' />
-      <button type='submit'>add new item</button>
-    </form>
+    <div className='form__wrapper'>
+      <h2>Add new twitter account</h2>
+      <form className='form__form' onSubmit={submitFn}>
+        <div className='form__item'>
+          <input placeholder='name' name='name' type='text' />
+        </div>
+        <div className='form__item'>
+          <input placeholder='link' name='link' type='text' />
+        </div>
+        <div className='form__item'>
+          <input placeholder='image' name='image' type='text' />
+        </div>
+        <div className='form__item'>
+          <textarea placeholder='description' name='description' />
+        </div>
+        <div className='form__item'>
+          <button type='submit'>add new item</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ListItem.css';
 
+const defaultImage =
+  'https://www.1lo.gorzow.pl/wp-content/uploads/2019/11/user-no-name.png';
+
 const ListItem = ({ image, name, description, twitterLink }) => {
   return (
     <li className='listItem___wrapper'>
-      <img src={image} alt={name} className='listItem__image' />
+      <img src={image || defaultImage} alt={name} className='listItem__image' />
       <div>
         <h2 className='listItem__name'>{name}</h2>
         <p className='listItem__description'>{description}</p>
@@ -23,7 +26,7 @@ const ListItem = ({ image, name, description, twitterLink }) => {
 };
 
 ListItem.propTypes = {
-  image: PropTypes.string.isRequired,
+  image: PropTypes.string,
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
   twitterLink: PropTypes.string.isRequired,
